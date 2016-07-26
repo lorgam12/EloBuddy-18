34be25acadff2282a1b565a8be30a85169e19c1f
stanixdor <aritzenil@hotmail.com>
2016-07-26 18:20:08 +0200

    Add files via upload
    SATANIX Katarina first release, (didn't even tested it)

---
+++ SATANIX Katarina/Config.cs
@@ -0,0 +1,93 @@
+﻿using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using Color = System.Drawing.Color;
+
+// ReSharper disable ConvertPropertyToExpressionBody
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberHidesStaticFromOuterClass
+
+namespace DamageIndicator
+{
+    public static class Config
+    {
+        private const string MenuName = "Damage Draw";
+
+        private static readonly Menu Menu;
+
+        static Config()
+        {
+            Menu = MainMenu.AddMenu(MenuName, MenuName.ToLower());
+            Menu.AddGroupLabel("Damage Draw");
+            Modes.Initialize();
+        }
+
+        public static void Initialize()
+        {
+        }
+
+        public static class Modes
+        {
+            private static readonly Menu DamageIndicatorMenu;
+
+            static Modes()
+            {
+
+                DamageIndicatorMenu = Menu.AddSubMenu("::DamageIndicator::");
+                Draw.Initialize();
+            }
+
+            public static void Initialize()
+            {
+            }
+
+            public static class Draw
+            {
+                private static readonly CheckBox _drawHealth;
+                private static readonly CheckBox _drawPercent;
+                private static readonly CheckBox _drawStatiscs;
+                //Color Config
+                private static readonly ColorConfig _healthColor;
+
+                //CheckBoxes
+
+                public static bool DrawHealth
+                {
+                    get { return _drawHealth.CurrentValue; }
+                }
+
+                public static bool DrawPercent
+                {
+                    get { return _drawPercent.CurrentValue; }
+                }
+
+                public static bool DrawStatistics
+                {
+                    get { return _drawStatiscs.CurrentValue; }
+                }
+
+
+                //Colors
+                public static Color HealthColor
+                {
+                    get { return _healthColor.GetSystemColor(); }
+                }
+
+                static Draw()
+                {
+                    DamageIndicatorMenu.AddGroupLabel("Spell drawings Settings :");
+                    _drawHealth = DamageIndicatorMenu.Add("damageIndicatorDraw", new CheckBox("Draw damage indicator ?"));
+                    _drawPercent = DamageIndicatorMenu.Add("percentageIndicatorDraw", new CheckBox("Draw damage percentage ?"));
+                    _drawStatiscs = DamageIndicatorMenu.Add("statiscsIndicatorDraw", new CheckBox("Draw damage statistics ?"));
+
+                    _healthColor = new ColorConfig(DamageIndicatorMenu, "healthColorConfig", Color.Yellow,
+                        "Color Damage Indicator:");
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+        }
+    }
+}
---
+++ SATANIX Katarina/killsteal.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+
+namespace SupaKS
+{
+    internal class KillSteal
+    {
+        public static bool UseQ
+        {
+            get { return SATANIXKatarina.Program.KillStealMenu["kQ"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static bool UseW
+        {
+            get { return SATANIXKatarina.Program.KillStealMenu["kW"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static bool UseE
+        {
+            get { return SATANIXKatarina.Program.KillStealMenu["kE"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static bool UseR
+        {
+            get { return SATANIXKatarina.Program.KillStealMenu["kR"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static void Execute()
+        {
+
+            if (Player.Instance.IsUnderTurret()) return;
+
+            var killableEnemies =
+                EntityManager.Heroes.Enemies.Where(
+                    t =>
+                        t.IsValidTarget() && !t.HasUndyingBuff() &&
+                        SATANIXKatarina.Damage.CalculateDamage(t, UseQ, UseW, UseE, UseR) >= t.Health);
+            var target = TargetSelector.GetTarget(killableEnemies, DamageType.Magical);
+
+            if (target == null) return;
+
+            if (UseQ &&
+                target.Health <= SATANIXKatarina.Damage.CalculateDamage(target, true, false, false, false))
+            {
+                CastQ(target);
+            }
+
+            else if (UseW &&
+                     target.Health <=
+                     SATANIXKatarina.Damage.CalculateDamage(target, false, true, false, false))
+            {
+                CastW(target);
+            }
+
+            else if (UseE &&
+                     target.Health <=
+                     SATANIXKatarina.Damage.CalculateDamage(target, false, false, true, false))
+            {
+                CastE(target);
+            }
+
+            else if (target.Health <=
+                     SATANIXKatarina.Damage.CalculateDamage(target, false, false, false, true))
+            {
+                CastR(target);
+            }
+
+        }
+
+        private static void CastQ(AIHeroClient target)
+        {
+            if (!SATANIXKatarina.Program.Q.IsReady()) return;
+
+            if (SATANIXKatarina.Program.Q.IsInRange(target))
+            {
+                SATANIXKatarina.Program.Q.Cast(target);
+            }
+        }
+
+
+        private static void CastW(AIHeroClient target)
+        {
+            if (!SATANIXKatarina.Program.W.IsReady()) return;
+
+            if (SATANIXKatarina.Program.W.IsInRange(target))
+            {
+                SATANIXKatarina.Program.W.Cast();
+            }
+        }
+
+        private static void CastE(AIHeroClient target)
+        {
+            if (!SATANIXKatarina.Program.E.IsReady()) return;
+
+            if (SATANIXKatarina.Program.E.IsInRange(target))
+            {
+                SATANIXKatarina.Program.E.Cast(target);
+            }
+        }
+
+
+        private static void CastR(AIHeroClient target)
+        {
+            if (!SATANIXKatarina.Program.R.IsReady()) return;
+
+            if (SATANIXKatarina.Program.E.IsInRange(target))
+            {
+                SATANIXKatarina.Program.R.Cast();
+            }
+
+        }
+    }
+}

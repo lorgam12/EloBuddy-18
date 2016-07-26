c9d26cb6589a87af591cb39cb7dc18594d9d58ce
stanixdor <aritzenil@hotmail.com>
2016-07-26 18:24:09 +0200

    Update AssemblyInfo.cs

--- SATANIX Katarina/Properties/AssemblyInfo.cs
+++ SATANIX Katarina/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SATANIXKatarina")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Smooth and high performance Katarina addon")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SATANIXKatarina")]

cea52e87cf09534e4fbba77dc1c42ce3813b7020
stanixdor <aritzenil@hotmail.com>
2016-08-03 22:26:38 +0200

    Added WardJump killsteal, cleaned WardJump code

---
+++ SATANIX Katarina/WardJumper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+namespace SATANIXKatarina
+{
+    internal class WardJumper
+    {
+        //WardJump
+        private static long _lastCheck;
+        public static long LastWard;
+        private static Vector3 _jumpPos;
+        public static Menu WardjumpMenu;
+        private static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        public static void Init()
+        {
+            Drawing.OnDraw += Drawing_OnDraw;
+
+            WardjumpMenu = Program.menu.AddSubMenu("WardJump Settings", "wardJumpSettings");
+            WardjumpMenu.AddGroupLabel("Wardjump Settings");
+            var a = WardjumpMenu.Add("alwaysMax", new CheckBox("Always Jump To Max Range"));
+            var b = WardjumpMenu.Add("onlyToCursor", new CheckBox("Always Jump To Cursor", false));
+            a.OnValueChange += delegate { if (a.CurrentValue) b.CurrentValue = false; };
+            b.OnValueChange += delegate { if (b.CurrentValue) a.CurrentValue = false; };
+            WardjumpMenu.AddSeparator();
+            WardjumpMenu.AddLabel("Time Modifications");
+            WardjumpMenu.Add("checkTime", new Slider("Position Reset Time (ms)", 0, 1, 2000));
+            WardjumpMenu.AddSeparator();
+            WardjumpMenu.AddLabel("Keybind Settings");
+
+            var wj = WardjumpMenu.Add("wardjumpKeybind",
+                new KeyBind("WardJump", false, KeyBind.BindTypes.HoldActive, 'T'));
+
+            WardjumpMenu.Add("drawWJ", new CheckBox("Draw WardJump"));
+
+            GameObject.OnCreate += GameObject_OnCreate;
+            Game.OnTick += delegate
+            {
+                if (wj.CurrentValue)
+                {
+                    WardJump(Game.CursorPos, a.CurrentValue, b.CurrentValue);
+                    return;
+                }
+            };
+        }
+
+        private static void GameObject_OnCreate(GameObject sender, EventArgs args)
+        {
+            if (sender.IsAlly && sender is Obj_AI_Base && sender.Name.ToLower().Contains("ward") && sender.Distance(_Player) < 600 && _jumpPos.Distance(sender) < 200)
+            {
+                Program.E.Cast((Obj_AI_Base) sender);
+            }
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (WardjumpMenu["drawWJ"].Cast<CheckBox>().CurrentValue && WardjumpMenu["wardjumpKeybind"].Cast<KeyBind>().CurrentValue)
+            {
+                Circle.Draw(Color.Teal, 100, _jumpPos);
+                Circle.Draw(Color.White, 600, _Player.Position);
+            }
+        }
+
+        public static void WardJump(Vector3 pos, bool max, bool cursorOnly)
+        {
+            if(WardjumpMenu["wardjumpKeybind"].Cast<KeyBind>().CurrentValue)
+                Orbwalker.OrbwalkTo(Game.CursorPos.Extend(Game.CursorPos, 200).To3D());
+            if (!_jumpPos.IsValid() || _lastCheck <= Environment.TickCount)
+            {
+                _jumpPos = pos;
+                _lastCheck = Environment.TickCount + WardjumpMenu["checkTime"].Cast<Slider>().CurrentValue;
+            }
+
+            var jumpPoint = _jumpPos;
+            if (max && jumpPoint.Distance(_Player.Position) > 600)
+            {
+                jumpPoint = _Player.Position.Extend(_jumpPos, 600).To3D();
+            }
+            else if (cursorOnly && jumpPoint.Distance(_Player.Position) > 600)
+            {
+                return;
+            }
+
+            _jumpPos = jumpPoint;
+            var ward =
+                ObjectManager.Get<Obj_AI_Base>()
+                    .FirstOrDefault(a => a.IsAlly && a.Distance(_jumpPos) < 100);
+            if (ward != null)
+            {
+                if (Program.E.IsReady())
+                {
+                    Player.CastSpell(SpellSlot.E, ward);
+                }
+            }
+            else
+            {
+                var wardSpot = GetWardSlot();
+                if (wardSpot == null)
+                {
+                    return;
+                }
+                if (Program.E.IsReady() && LastWard + 400 < Environment.TickCount)
+                {
+                    GetWardSlot().Cast(_jumpPos);
+                    LastWard = Environment.TickCount;
+                }
+            }
+        }
+
+        public static InventorySlot GetWardSlot()
+        {
+            var wardIds = new[] {ItemId.Warding_Totem_Trinket, ItemId.Sightstone, ItemId.Ruby_Sightstone, ItemId.Vision_Ward, ItemId.Greater_Stealth_Totem_Trinket};
+            return _Player.InventoryItems.FirstOrDefault(a => wardIds.Contains(a.Id) && a.IsWard && a.CanUseItem());
+        }
+    }
+}
---
+++ SATANIX Katarina/killsteal.cs
@@ -0,0 +1,137 @@
+﻿using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+
+namespace SupaKS
+{
+    internal class KillSteal
+    {
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+        public static bool UseQ
+        {
+            get { return SATANIXKatarina.Program.KillStealMenu["kQ"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static bool UseW
+        {
+            get { return SATANIXKatarina.Program.KillStealMenu["kW"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static bool UseE
+        {
+            get { return SATANIXKatarina.Program.KillStealMenu["kE"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static bool UseR
+        {
+            get { return SATANIXKatarina.Program.KillStealMenu["kR"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static void Execute()
+        {
+
+            if (Player.Instance.IsUnderTurret()) return;
+
+            var killableEnemies =
+                EntityManager.Heroes.Enemies.Where(
+                    t =>
+                        t.IsValidTarget() && !t.HasUndyingBuff() &&
+                        SATANIXKatarina.Damage.CalculateDamage(t, UseQ, UseW, UseE, UseR) >= t.Health);
+            var target = TargetSelector.GetTarget(killableEnemies, DamageType.Magical);
+
+            if (target == null) return;
+            if (UseQ &&
+                target.Health <= SATANIXKatarina.Damage.CalculateDamage(target, true, false, false, false))
+            {
+                CastQ(target);
+            }
+
+            else if (UseW &&
+                     target.Health <=
+                     SATANIXKatarina.Damage.CalculateDamage(target, false, true, false, false))
+            {
+                CastW(target);
+            }
+
+            else if (UseE &&
+                     target.Health <=
+                     SATANIXKatarina.Damage.CalculateDamage(target, false, false, true, false))
+            {
+                CastE(target);
+            }
+
+            else if (target.Health <=
+                     SATANIXKatarina.Damage.CalculateDamage(target, false, false, false, true))
+            {
+                CastR(target);
+            }
+
+        }
+
+        private static void CastQ(AIHeroClient target)
+        {
+            if (!SATANIXKatarina.Program.Q.IsReady()) return;
+            if (SATANIXKatarina.Program.Q.IsInRange(target))
+            {
+                if (SATANIXKatarina.StateManager.HumanizerMenu["Humanizer"].Cast<CheckBox>().CurrentValue)
+                    Core.DelayAction(() => SATANIXKatarina.Program.Q.Cast(target), SATANIXKatarina.StateManager.HumanizerMenu["QDelay"].Cast<Slider>().CurrentValue);
+                else
+                    SATANIXKatarina.Program.Q.Cast(target);
+            }
+            else if ((target.Distance(_Player) < (SATANIXKatarina.Program.Q.Range + 600)) && SATANIXKatarina.Program.KillStealMenu["kWardJump"].Cast<CheckBox>().CurrentValue)
+            {
+                SATANIXKatarina.WardJumper.WardJump(target.Position, true, false);
+            }
+        }
+
+
+        private static void CastW(AIHeroClient target)
+        {
+            if (!SATANIXKatarina.Program.W.IsReady()) return;
+
+            if (SATANIXKatarina.Program.W.IsInRange(target))
+            {
+                if (SATANIXKatarina.StateManager.HumanizerMenu["Humanizer"].Cast<CheckBox>().CurrentValue)
+                    Core.DelayAction(() => SATANIXKatarina.Program.W.Cast(), SATANIXKatarina.StateManager.HumanizerMenu["WDelay"].Cast<Slider>().CurrentValue);
+                else
+                    SATANIXKatarina.Program.W.Cast();
+            }
+            else if ((target.Distance(_Player) < (SATANIXKatarina.Program.W.Range + 600)) && SATANIXKatarina.Program.KillStealMenu["kWardJump"].Cast<CheckBox>().CurrentValue)
+            {
+                SATANIXKatarina.WardJumper.WardJump(target.Position, true, false);
+            }
+        }
+
+        private static void CastE(AIHeroClient target)
+        {
+            if (!SATANIXKatarina.Program.E.IsReady()) return;
+
+            if (SATANIXKatarina.Program.E.IsInRange(target))
+            {
+                if (SATANIXKatarina.StateManager.HumanizerMenu["Humanizer"].Cast<CheckBox>().CurrentValue)
+                    Core.DelayAction(() => SATANIXKatarina.Program.E.Cast(target), SATANIXKatarina.StateManager.HumanizerMenu["EDelay"].Cast<Slider>().CurrentValue);
+                else
+                    SATANIXKatarina.Program.E.Cast(target);
+            }
+        }
+
+
+        private static void CastR(AIHeroClient target)
+        {
+            if (!SATANIXKatarina.Program.R.IsReady()) return;
+
+            if (SATANIXKatarina.Program.E.IsInRange(target))
+            {
+                if (SATANIXKatarina.StateManager.HumanizerMenu["Humanizer"].Cast<CheckBox>().CurrentValue)
+                    Core.DelayAction(() => SATANIXKatarina.Program.R.Cast(), SATANIXKatarina.StateManager.HumanizerMenu["RDelay"].Cast<Slider>().CurrentValue);
+                else
+                    SATANIXKatarina.Program.R.Cast();
+            }
+
+        }
+    }
+}

04ee788029969a8a99f3957451c00fd6492be705
stanixdor <aritzenil@hotmail.com>
2016-08-07 19:19:19 +0200

    Kurisu Riven (my ported version)

---
+++ KurisuRiven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using LeagueSharp.Common;
+using EloBuddy;
+
+namespace KurisuRiven
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            new KurisuRiven();
+        }
+    }
+}
